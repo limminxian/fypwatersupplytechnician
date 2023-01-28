c843086eae5a205db1c018dc9016d69272a03381
hasnaruhi <hasna.ruhi16@gmail.com>
2023-01-28 12:15:33 +0800

    Update login.php

--- Technician/login.php
+++ Technician/login.php
@@ -5,16 +5,23 @@
         $result = array();
         $conn = mysqli_connect("us-cdbr-east-06.cleardb.net", "bbd12ae4b2fcc3", "df9ea7aa", "heroku_80d6ea926f679b3");
         if ($conn) {
-            $sql = "select * from USERS where EMAIL = '" . $EMAIL . "'";
+            //$sql = "select * from USERS where EMAIL = '" . $EMAIL . "'";
+            $sql = "select u.NAME, u.EMAIL, u.PASSWORD, u.TYPE, u.STATUS, r.name as ROLENAME from users u
+            inner join roles r on u.TYPE = r.ID
+            where u.EMAIL = '" . $EMAIL . "'";
             $res = mysqli_query($conn, $sql);
             if (mysqli_num_rows($res) != 0) {
                 $row = mysqli_fetch_assoc($res); 
                 if (mysqli_query($conn, $sql)) {
-                    $result = array("status" => "success", "message" => "Login successful",
-                        "NAME" => $row['NAME'], "EMAIL" => $row['EMAIL'], "PASSWORD" => $row['PASSWORD']);
+                    if($row['ROLENAME'] == 'Technician' || $row['ROLENAME'] == 'technician'|| $row['ROLENAME'] == 'TECHNICIAN') {
+                        if($row['STATUS'] == 'ACTIVE') {
+                            $result = array("status" => "success", "message" => "Login successful",
+                            "NAME" => $row['NAME'], "EMAIL" => $row['EMAIL'], "PASSWORD" => $row['PASSWORD'], "ROLENAME" => $row['ROLENAME'], "STATUS" => $row['STATUS']);
+                        } else $result = array("status" => "failed", "message" => "You have been suspended");
+                    } else $result = array("status" => "failed", "message" => "You are not a technician to log in");
                 } else $result = array("status" => "failed", "message" => "Login failed try again");
             } else $result = array("status" => "failed", "message" => "Retry with correct email and password");
         } else $result = array("status" => "failed", "message" => "Database connection failed");
     } else $result = array("status" => "failed", "message" => "All fields are required");
     echo json_encode($result, JSON_PRETTY_PRINT);
-?> 
+?>
