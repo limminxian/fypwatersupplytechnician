db53ba5f5059f31a0f834182080b74e5f1b37c80
hasnaruhi <hasna.ruhi16@gmail.com>
2023-02-08 18:51:21 +0800

    heroku 2nd database

--- Technician/editWaterUsage.php
+++ Technician/editWaterUsage.php
@@ -1,6 +1,7 @@
 <?php
 if (!empty($_POST['homeownerId'] && !empty($_POST['waterUsage']))) {
-    $conn = mysqli_connect("us-cdbr-east-06.cleardb.net", "bbd12ae4b2fcc3", "df9ea7aa", "heroku_80d6ea926f679b3");
+    $conn = mysqli_connect("us-cdbr-east-06.cleardb.net", "bc292174f8cae7", "68916e25", "heroku_a43ceec7a5c075b");
+    //$conn = mysqli_connect("us-cdbr-east-06.cleardb.net", "bbd12ae4b2fcc3", "df9ea7aa", "heroku_80d6ea926f679b3");
     //$conn = mysqli_connect("localhost", "root", "", "fyp");
     $id = $_POST['homeownerId'];
     $waterUsage = $_POST['waterUsage'];
