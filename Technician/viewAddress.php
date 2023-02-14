0c260b734a286d81d4d7c50bff970b99b7dee9ae
hasnaruhi <hasna.ruhi16@gmail.com>
2023-02-15 00:42:14 +0800

    testttt

--- Technician/viewAddress.php
+++ Technician/viewAddress.php
@@ -4,7 +4,7 @@
     //$conn = mysqli_connect("localhost", "root", "", "fyp");
     if($conn) {
         $selectedarea = $_GET['area'];
-        $sql = "select h.ID, h.STREET, h.BLOCKNO, h.UNITNO, h.POSTALCODE, h.AREA, h.HOUSETYPE, h.SUBSCRIBE, u.ID
+        $sql = "select h.ID, h.STREET, h.BLOCKNO, h.UNITNO, h.POSTALCODE, h.AREA, h.HOUSETYPE, h.SUBSCRIBE
             from homeowner h
             inner join company c on h.SUBSCRIBE = c.ID
             where h.area = '$selectedarea'
